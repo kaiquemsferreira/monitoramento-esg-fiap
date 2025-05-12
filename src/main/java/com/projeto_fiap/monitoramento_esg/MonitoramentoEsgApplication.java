b9507fdd6135f9fb83991f8d05a18db45910316c
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-05-12 18:59:43 -0300

    First commit

---
+++ src/main/java/com/projeto_fiap/monitoramento_esg/MonitoramentoEsgApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_fiap.monitoramento_esg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoEsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoEsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_fiap/monitoramento_esg/MonitoramentoEsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_fiap.monitoramento_esg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoEsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
