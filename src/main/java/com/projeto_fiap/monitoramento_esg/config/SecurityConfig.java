d4fd01ae9da8c9a9af9e0afb68aae89de12dcd3d
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-05-13 18:15:56 -0300

    Implementação do liquibase

--- src/main/java/com/projeto_fiap/monitoramento_esg/config/SecurityConfig.java
+++ src/main/java/com/projeto_fiap/monitoramento_esg/config/SecurityConfig.java
@@ -18,8 +18,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/favicon.ico", "/v3/api-docs/**", "/swagger-ui.html",
-                                "/swagger-ui/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**",
+                                "/swagger-resources/**", "/webjars/**").permitAll()
 
                         .anyRequest().authenticated()
                 )
