63a993496af6251a9b23fe6f6c1269f777c6ca03
kaique.ferreira <kaique.ferreira.terc@preventsenior.com.br>
2025-05-13 18:22:41 -0300

    Ajustando descrição swagger

--- src/main/java/com/projeto_fiap/monitoramento_esg/config/OpenAPIConfig.java
+++ src/main/java/com/projeto_fiap/monitoramento_esg/config/OpenAPIConfig.java
@@ -15,12 +15,18 @@ public class OpenAPIConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("ESG Energy Monitoring API")
-                        .description("API REST para monitoramento de sensores de consumo energético e alertas de consumo excessivo")
-                        .description("Projeto com o intuito acadêmico referente a um projeto da Faculdade de Informática e Administração Paulista (FIAP)")
+                        .description("""
+                        API REST para monitoramento de sensores de consumo energético e alertas de consumo excessivo.
+
+                        Projeto com intuito acadêmico referente a um projeto da Faculdade de Informática e Administração Paulista (FIAP).
+
+                        Contatos:
+                        - Kaique Martins da Silva Ferreira: kaiquemartinsdasilvaferreira@gmail.com (RM557522)
+                        - Henrique de Novaes: rick.novaes10@gmail.com (RM98485)
+                        - Mauricio Dias Gonçalves: mauriciodiasgoncalves@gmail.com (RM555486)
+                        """)
                         .version("1.0")
                         .contact(new Contact().name("Kaique Martins da Silva Ferreira").email("kaiquemartinsdasilvaferreira@gmail.com"))
-                        .contact(new Contact().name("Henrique de Novaes").email("rick.novaes10@gmail.com"))
-                        .contact(new Contact().name("Mauricio Dias Gonçalves").email("mauriciodiasgoncalves@gmail.com"))
                         .license(new License().name("Uso acadêmico")));
     }
 }
